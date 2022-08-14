7b686d8db3bc31d2ace40e8911a307bd6419e00a
DevMartinese <devmartinese@gmail.com>
2022-08-14 01:23:44 -0300

    add testing

--- src/test/unit/controllerUnit.test.js
+++ src/test/unit/controllerUnit.test.js
@@ -3,15 +3,19 @@ const { getLocationByIp } = require('../../services/getLocationByIp');
 const { getWeatherByCity } = require('../../services/getWeatherByCity');
 const { getForecastByCity } = require('../../services/getForecastByCity');
 
+const mockLocation = require('../mocks/locationMock.json');
+const mockWeather = require('../mocks/weatherMock.json');
+const mockForecast = require('../mocks/forecastMock.json');
+const mockResponseLocation = require('../mocks/locationResponseMock.json');
+const mockResponseWeather = require('../mocks/weatherResponseMock.json');
+const mockResponseForecast = require('../mocks/forecastResponseMock.json');
+
 const httpMocks = require('node-mocks-http');
 const { createRequest, createResponse } = httpMocks;
 
-const mockLocation = require('../mocks/locationMock.json');
-const mockResponseLocation = require('../mocks/locationResponseMock.json');
-const mockResponseWeather = require('../mocks/weatherResponseMock.json');
-
 jest.mock('../../services/getLocationByIp');
 jest.mock('../../services/getWeatherByCity');
+jest.mock('../../services/getForecastByCity');
 
 describe('getDataByLocation', () => {
   it('should return data by location', async () => {
@@ -31,6 +35,64 @@ describe('getDataByLocation', () => {
     const error = new Error('Error');
     getLocationByIp.mockRejectedValue(error);
     await getDataByLocation(req, res);
+    expect(res.statusCode).toBe(500);
+  });
+});
+
+describe('getWeatherByLocation', () => {
+  it('should return weather by location', async () => {
+    const req = createRequest({
+      params: {
+        city: 'London',
+      }
+    });
+    const res = createResponse();
+    const weather = mockWeather;
+    getWeatherByCity.mockResolvedValue(weather);
+    await getWeatherByLocation(req, res);
+    expect(res._getJSONData()).toEqual(mockResponseWeather);
+    expect(res.statusCode).toBe(200);
+  });
+
+  it('should return error', async () => {
+    const req = createRequest({
+      params: {
+        city: 'London',
+      }
+    });
+    const res = createResponse();
+    const error = new Error('Error');
+    getWeatherByCity.mockRejectedValue(error);
+    await getWeatherByLocation(req, res);
+    expect(res.statusCode).toBe(500);
+  });
+});
+
+describe('getForecastByLocation', () => {
+  it('should return forecast by location', async () => {
+    const req = createRequest({
+      params: {
+        city: 'London',
+      }
+    });
+    const res = createResponse();
+    const forecast = mockForecast;
+    getForecastByCity.mockResolvedValue(forecast);
+    await getForecastByLocation(req, res);
+    expect(res._getJSONData()).toEqual(mockResponseForecast);
+    expect(res.statusCode).toBe(200);
+  });
+
+  it('should return error', async () => {
+    const req = createRequest({
+      params: {
+        city: 'London',
+      }
+    });
+    const res = createResponse();
+    const error = new Error('Error');
+    getForecastByCity.mockRejectedValue(error);
+    await getForecastByLocation(req, res);
     expect(res.statusCode).toBe(500);
   });
 });
