f7ca402e7bac1293cab263172feae177be30bf3b
DevMartinese <devmartinese@gmail.com>
2022-08-14 03:12:05 -0300

    add testing

---
+++ src/test/integration/controllerIntegration.test.js
@@ -0,0 +1,40 @@
+const request = require('supertest');
+const app = require('../../index');
+const city = 'London';
+
+describe('getDataByLocation integration', () => {
+  it('should return data by location', async () => {
+    await request(app).get('/v1/location')
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200)
+  });
+
+  it('should return weather data by location by ipapi', async () => {
+    await request(app).get('/v1/current')
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200)
+  });
+
+  it('should return weather data by location param', async () => {
+    await request(app).get(`/v1/current/${city}`)
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200)
+  });
+
+  it('should return forecast data by location by ipapi', async () => {
+    await request(app).get('/v1/forecast')
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200)
+  });
+
+  it('should return forecast data by location param', async () => {
+    await request(app).get(`/v1/forecast/${city}`)
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200)
+  });
+});
